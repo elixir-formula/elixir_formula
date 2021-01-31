c6d78794f969cbc7b9cba4a65d474f5e8ec1c5d9
drEnilight <vladyslav.romaniuk@outlook.com>
2021-01-31 13:44:40 +0200

    Publication#create_changeset tests

--- test/elixir_formula/publications/publication_test.exs
+++ test/elixir_formula/publications/publication_test.exs
@@ -15,9 +15,17 @@ defmodule ElixirFormula.PublicationTest do
   end
 
   describe "create_changeset/2" do
-    test "success case", %{base_params: base_params} do
+    test "returns valid changeset", %{base_params: base_params} do
       changeset = Publication.create_changeset(%Publication{}, base_params)
-      assert %{} == errors_on(changeset)
+
+      assert errors_on(changeset) == %{}
+    end
+
+    test "returns invalid chaangeset, when title isn't present", %{base_params: base_params} do
+      invalid_params = Map.merge(base_params, %{title: nil})
+      changeset = Publication.create_changeset(%Publication{}, invalid_params)
+
+      assert errors_on(changeset) == %{title: ["can't be blank"]}
     end
   end
 end
