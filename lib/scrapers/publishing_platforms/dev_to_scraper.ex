a96bcf49bb2afd4f18bc5f82205d02f76a37f832
drEnilight <vladyslav.romaniuk@outlook.com>
2021-02-14 15:31:54 +0200

    add Dev.to scraper

---
+++ lib/scrapers/publishing_platforms/dev_to_scraper.ex
@@ -0,0 +1,38 @@
+defmodule Scrapers.PublishingPlatforms.DevToScraper do
+  use Scrapers.Interface
+
+  def article_author(article) do
+    article
+    |> Floki.find("p > a.crayons-story__secondary.fw-medium")
+    |> Floki.text
+    |> String.trim
+  end
+
+  def article_title(article) do
+    article
+    |> Floki.find("h2.crayons-story__title")
+    |> Floki.text
+    |> String.trim
+  end
+
+  def article_tags(article) do
+    article
+    |> Floki.find("a.crayons-tag")
+    |> Floki.text
+    |> String.split("#", trim: true)
+  end
+
+  def article_url(article) do
+    url =
+      article
+      |> Floki.find("h2.crayons-story__title > a")
+      |> Floki.attribute("href")
+      |> Floki.text
+
+    "https://dev.to" <> url
+  end
+
+  def article_source, do: "dev.to"
+
+  def resource, do: "https://dev.to/t/elixir/latest"
+end
