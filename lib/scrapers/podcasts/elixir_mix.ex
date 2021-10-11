12e449961f641d2fecb33abc4aa357543b01a52e
drEnilight <vladyslav.romaniuk@outlook.com>
2021-10-11 19:22:48 +0300

    update elixir_mix scraper

--- lib/scrapers/podcasts/elixir_mix.ex
+++ lib/scrapers/podcasts/elixir_mix.ex
@@ -7,27 +7,28 @@ defmodule Scrapers.Podcasts.ElixirMix do
   # articles processing
 
   def resource,
-    do: "https://devchat.tv/elixir-mix/"
+    do: "https://elixirmix.com/"
 
   def article_source,
     do: "elixir_mix"
 
   def articles_selector,
-    do: "div.secondline-masonry-padding-blog"
+    do: "ul.list.cover > li"
 
   def article_url(article) do
-    article
-    |> Floki.find("h2.secondline-blog-title > a")
-    |> Floki.attribute("href")
-    |> Floki.text()
+    url =
+      article
+      |> Floki.find("h2.secondline-blog-title > a")
+      |> Floki.attribute("href")
+      |> Floki.text()
+
+    "https://elixirmix.com" <> url
   end
 
   # single article processing
 
-  def article_author(article) do
-    article
-    |> Floki.find("span.blog-meta-author-display > a")
-    |> Floki.text()
+  def article_author(_article) do
+    "elixir_mix"
   end
 
   def article_image_url(article) do
