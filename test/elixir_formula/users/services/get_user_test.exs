e2beaebec2a48d13c27b19cda465249d691dd803
drEnilight <vladyslav.romaniuk@outlook.com>
2022-01-24 14:34:54 +0200

    test commit 1

--- test/elixir_formula/users/services/get_user_test.exs
+++ test/elixir_formula/users/services/get_user_test.exs
@@ -16,16 +16,16 @@ defmodule ElixirFormula.Users.Services.GetUserTest do
     end
   end
 
-  describe "when searching by email" do
-    test "returns {:ok, %User{}} when user exists" do
-      user = insert(:user)
-
-      assert {:ok, new_user} = GetUser.call(:email, user.email)
-      assert new_user.id == user.id
-    end
-
-    test "returns error when user doesn't exist" do
-      assert {:error, :user_not_found} = GetUser.call(:email, "fake_email@gmail.com")
-    end
-  end
+  # describe "when searching by email" do
+  #   test "returns {:ok, %User{}} when user exists" do
+  #     user = insert(:user)
+  #
+  #     assert {:ok, new_user} = GetUser.call(:email, user.email)
+  #     assert new_user.id == user.id
+  #   end
+  #
+  #   test "returns error when user doesn't exist" do
+  #     assert {:error, :user_not_found} = GetUser.call(:email, "fake_email@gmail.com")
+  #   end
+  # end
 end
